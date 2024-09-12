84baf3afdf96fe68e3bf72bc1cb85b960b59fe44
RaghdaRamadan2000 <rr1368@fayoum.edu.eg>
2024-09-13 01:06:36 +0300

    frist

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace darft
+{
+    internal class Program
+    {
+
+        public static IEnumerable<int> f()
+        {
+            Console.WriteLine("dd");
+            yield return 1;
+            Console.WriteLine("11");
+            yield return 2;
+            Console.WriteLine("cc");
+            yield return 3;
+        }
+        static void Main(string[] args)
+        {
+            IEnumerable<int> l= f();
+            
+            foreach (int i in l)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
